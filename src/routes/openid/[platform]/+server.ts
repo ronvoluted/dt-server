625c358c345ead05e586f54ace5fc618320fa326
Ron Au <5785323+ronvoluted@users.noreply.github.com>
2022-12-26 19:09:09 +1000

    Initial D4

--- src/routes/openid/[platform]/+server.ts
+++ src/routes/openid/[platform]/+server.ts
@@ -4,24 +4,24 @@ import { json } from '@sveltejs/kit';
 import { entriesToObject } from '$lib//utility';
 
 const openid: RequestHandler = async ({ params, url }) => {
-	const openid = entriesToObject(url.searchParams.entries());
+	const inputParams = entriesToObject(url.searchParams.entries());
 
-	console.log(openid);
+	console.log(inputParams);
 
 	const steamWebUrl = new URL('https://accounts.atoma.cloud/linking');
 
 	// @ts-expect-error
 	const steamWebParams = new URLSearchParams({
-		"openid.ns": openid.ns,
-		"openid.mode": openid.mode,
-		"openid.op_endpoint": openid.op_endpoint,
-		"openid.claimed_id": openid.identity,
-		"openid.identity": openid.identity,
-		"openid.response_nonce": openid.response_nonce,
-		"openid.assoc_handle": openid.assoc_handle,
-		"openid.invalidate_handle": openid.invalidate_handle,
-		"openid.signed": openid.signed,
-		"openid.sig": openid.sig,
+		"openid.ns": inputParams['openid.ns'],
+		"openid.mode": inputParams['openid.mode'],
+		"openid.op_endpoint": inputParams['openid.op_endpoint'],
+		"openid.claimed_id": inputParams['openid.identity'],
+		"openid.identity": inputParams['openid.identity'],
+		"openid.response_nonce": inputParams['openid.response_nonce'],
+		"openid.assoc_handle": inputParams['openid.assoc_handle'],
+		"openid.invalidate_handle": inputParams['openid.invalidate_handle'],
+		"openid.signed": inputParams['openid.signed'],
+		"openid.sig": inputParams['openid.sig'],
 		"openid.return_to": "https://accounts.atoma.cloudz/linking",
 	});
 
