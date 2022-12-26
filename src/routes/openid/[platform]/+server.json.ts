ab6c2057a2eb7b22772c36ef9bbae463f51dd4c5
Ron Au <5785323+ronvoluted@users.noreply.github.com>
2022-12-26 19:31:59 +1000

    Initial D5

--- src/routes/openid/[platform]/+server.json.ts
+++ src/routes/openid/[platform]/+server.json.ts
@@ -8,29 +8,31 @@ const openid: RequestHandler = async ({ params, url }) => {
 
 	console.log(inputParams);
 
-	const steamWebUrl = new URL('https://accounts.atoma.cloud/linking');
+	const platformUrl = new URL('https://accounts.atoma.cloud/linking');
 
 	// @ts-expect-error
-	const steamWebParams = new URLSearchParams({
+	const platformParams = new URLSearchParams({
 		"openid.ns": inputParams['openid.ns'],
 		"openid.mode": inputParams['openid.mode'],
 		"openid.op_endpoint": inputParams['openid.op_endpoint'],
 		"openid.claimed_id": inputParams['openid.identity'],
 		"openid.identity": inputParams['openid.identity'],
+		"openid.return_to": "https://accounts.atoma.cloud/linking",
 		"openid.response_nonce": inputParams['openid.response_nonce'],
 		"openid.assoc_handle": inputParams['openid.assoc_handle'],
 		"openid.invalidate_handle": inputParams['openid.invalidate_handle'],
 		"openid.signed": inputParams['openid.signed'],
 		"openid.sig": inputParams['openid.sig'],
-		"openid.return_to": "https://accounts.atoma.cloudz/linking",
 	});
 
-	steamWebUrl.search = steamWebParams.toString();
+	platformUrl.search = platformParams.toString();
 
-	console.log({ atomaJoinUrl: steamWebUrl.href });
+	console.log({ atomaJoinUrl: platformUrl.href });
 
 	return json({
-		Authorization: `${params.platform} ${steamWebUrl}`,
+		platformUrl: platformUrl,
+		Authorization: `${params.platform} ${platformUrl}`,
+		inputParams,
 	});
 };
 
